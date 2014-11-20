ef9707f35805bade5a2cd00dcbb108fdd677a370
fzerorubigd <fzerorubigd@gmail.com>
2014-11-20 14:15:46 +0330

    fix a simple problem with $ in bash

--- user.sh
+++ user.sh
@@ -12,6 +12,6 @@ sudo -Hu develop -- wget -O /home/develop/.zshrc http://git.grml.org/f/grml-etc-
 sudo -Hu develop -- wget -O /home/develop/.zshrc.local  http://git.grml.org/f/grml-etc-core/etc/skel/.zshrc
 
 # Go related stuff
-bash -c "echo export GOPATH=/home/develop >> /etc/environment"
+bash -c "echo export GOPATH=/home/develop > /etc/environment"
 sudo -Hu develop -- bash -c "echo export GOPATH=/home/develop >> /home/develop/.zshrc.local"
-sudo -Hu develop -- bash -c "echo export PATH=$PATH:/usr/local/go/bin:/home/develop/bin >> /home/develop/.zshrc.local"
+sudo -Hu develop -- bash -c "echo export PATH=\$PATH:/usr/local/go/bin:/home/develop/bin >> /home/develop/.zshrc.local"
